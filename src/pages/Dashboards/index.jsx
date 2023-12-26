ab146dd28464fe67c0fcdefa41b0b2f9ae605051
ErlisonGuaraves <erlison.santos@guaraves.com.br>
2023-12-26 10:54:37 -0300

    resolvido o problema da mensagem

--- src/pages/Dashboards/index.jsx
+++ src/pages/Dashboards/index.jsx
@@ -1,82 +1,60 @@
-import { useEffect, useState } from "react"
-import { db } from "../../connection/firebase"
+import { useEffect, useState } from "react";
+import { db } from "../../connection/firebase";
 import { collection, query, getDocs } from "firebase/firestore";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import CardComponent from "../../components/Card"
+import CardComponent from "../../components/Card";
 import { useContext } from "react";
 import ModalContext from "../../contexts/ModalContext";
 import ModalComponent from "../../components/Modal";
 
-import "./styles.css"
+import "./styles.css";
 import Spinner from 'react-bootstrap/Spinner';
 
-
 const Dashboards = () => {
-
-  const [dashs, setDashs] = useState([])
-
-  console.log(dashs)
-
-  const [showModal, setShowModal] = useContext(ModalContext)
-  const [showMessage, setShowMessage] = useState(false)
-
-  console.log(showModal)
+  const [dashs, setDashs] = useState([]);
+  const [showModal, setShowModal] = useContext(ModalContext);
 
   const handleShow = () => setShowModal(true);
   const handleClose = () => setShowModal(false);
 
   useEffect(() => {
-    async function getData(){
-      try{
-          console.log("no try")
-          setDashs([])
-          setShowMessage(false)
-    
-          const q = query(collection(db, "dashboards"));
-          const querySnapshot = await getDocs(q);
-          let dados = [];
-  
-  
-          querySnapshot.forEach((doc) => {
-              dados.push({
-                "id": doc.id,
-                "dados": doc.data()
-              })
+    async function getData() {
+      try {
+        const q = query(collection(db, "dashboards"));
+        const querySnapshot = await getDocs(q);
+        let dados = [];
+
+        querySnapshot.forEach((doc) => {
+          dados.push({
+            id: doc.id,
+            dados: doc.data()
           });
+        });
 
-          setDashs(dados)      
-
-
-  
-      }catch(err){
-        console.log(err)
-        console.log("no catch")
+        setDashs(dados);
+      } catch (err) {
+        console.log(err);
       }
     }
-      
-    getData()
-  }, [])
 
+    getData();
+  }, []);
 
-  console.log(showMessage)
-
-  return(
+  return (
     <main id="container">
-      <Header handleShow={handleShow}/>
-      <ModalComponent setDashs={setDashs} show={showModal} handleClose={handleClose}/>
-      <div id="container-dashboards" >
-      {dashs && dashs.map(dash => (
-              <CardComponent dash={dash} setDashs={setDashs} />
-          ))
-        }
-       {showMessage && (<h1>Nenhum dashboard encontrado</h1>)}
-            
+      <Header handleShow={handleShow} />
+      <ModalComponent setDashs={setDashs} show={showModal} handleClose={handleClose} />
+      <div id="container-dashboards">
+        {dashs.length > 0 ? (
+          dashs.map((dash) => <CardComponent dash={dash} setDashs={setDashs} />)
+        ) : (
+          <h1>Nenhum dashboard encontrado</h1>
+        )}
       </div>
       <Footer />
-   </main>
-  )
-}
-
+    </main>
+  );
+};
 
 export default Dashboards;
