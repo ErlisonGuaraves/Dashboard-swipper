a654ad138e198ba96a11c47e84bdae65c5950e52
ErlisonGuaraves <erlison.santos@guaraves.com.br>
2023-12-27 09:54:21 -0300

    removido botão sem funcionalidade

--- src/components/SearchFilter/index.jsx
+++ src/components/SearchFilter/index.jsx
@@ -1,5 +1,5 @@
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
@@ -8,7 +8,6 @@ import "./style.css";
 const SearchFilter = ({ search, setSearch }) => {
   return (
     <div id="search" className="mt-4">
-      <Form inline>
         <Row className="justify-content-end">
           <Col xs="auto">
             <Form.Control
@@ -20,12 +19,8 @@ const SearchFilter = ({ search, setSearch }) => {
             />
           </Col>
           <Col xs="auto" className="custom-mr">
-            {" "}
-            {/* Adicione a classe custom-mr para ajustar a margem à direita */}
-            <Button type="submit">buscar</Button>
           </Col>
         </Row>
-      </Form>
     </div>
   );
 };
